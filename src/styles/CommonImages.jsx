98bf499800a5368391f7471a20a7a29633f3a08a
minzyee <vvzmsqufvv@gmail.com>
2023-02-12 16:15:40 +0900

    assets: 감정 이미지 파일 추가

---
+++ src/styles/CommonImages.jsx
@@ -0,0 +1,23 @@
+import FACE_CUTE from "../assets/images/face_깜찍.svg";
+import FACE_PANIC from "../assets/images/face_당황.svg";
+import FACE_THINK from "../assets/images/face_머엉.svg";
+import FACE_SMILE from "../assets/images/face_미소.svg";
+import FACE_SHY from "../assets/images/face_수줍.svg";
+import FACE_SADNESS from "../assets/images/face_슬픔.svg";
+import FACE_FUN from "../assets/images/face_신남.svg";
+import FACE_RIDICULE from "../assets/images/face_썩소.svg";
+import FACE_SPARE from "../assets/images/face_여유.svg";
+import FACE_LAUGH from "../assets/images/face_웃음.svg";
+
+export {
+	FACE_CUTE,
+	FACE_PANIC,
+	FACE_THINK,
+	FACE_SMILE,
+	FACE_SHY,
+	FACE_SADNESS,
+	FACE_FUN,
+	FACE_RIDICULE,
+	FACE_SPARE,
+	FACE_LAUGH,
+};
